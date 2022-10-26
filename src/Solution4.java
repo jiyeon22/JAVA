da98d1d80e7d266ab13c2a2434ace76dd44d3713
jiyeon22 <kkll4106@naver.com>
2022-10-26 17:14:32 +0900

    수박수박수?

---
+++ src/Solution4.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/*
+ * 프로그래머스 lv1. 수박수박수 
+ */
+public class Solution4 {
+	
+
+		public static String solution(int n) {
+			String str1 = "수";
+			String str2 = "박";
+			String answer = "";
+			
+				for(int i = 1; i <= n; i++) {
+					if(i % 2 == 0) {
+						answer += str2;
+					}else answer += str1;
+					
+				}
+				return answer;
+			}
+			
+			
+			
+			
+			
+				
+	
+	public static void main(String[] args) {
+		
+
+		
+		System.out.println(solution(8));
+	}
+}
+
+
