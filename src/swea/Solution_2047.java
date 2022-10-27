1eeb2b7d3633a9117c3c6922c0720744fdd36691
jiyeon22 <kkll4106@naver.com>
2022-10-27 16:03:14 +0900

    신문 헤드라인

---
+++ src/swea/Solution_2047.java
@@ -0,0 +1,14 @@
+package swea;
+
+import java.util.Scanner;
+
+public class Solution_2047 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+        System.out.println(s.toUpperCase());
+	        
+	        
+	}
+
+}
