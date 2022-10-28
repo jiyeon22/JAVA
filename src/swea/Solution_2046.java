31496a13f2dc4bad0b57623d8e01a555310d9532
jiyeon22 <kkll4106@naver.com>
2022-10-28 10:51:32 +0900

    스탬프 찍기

---
+++ src/swea/Solution_2046.java
@@ -0,0 +1,18 @@
+package swea;
+
+import java.util.Scanner;
+
+public class Solution_2046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 0; i < N; i++) {
+			System.out.print("#");
+		}
+			
+		
+	}
+
+}
