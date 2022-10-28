b6178289e7276d8467dbc96ae6bc9c2a8369f47d
jiyeon22 <kkll4106@naver.com>
2022-10-28 10:57:10 +0900

    서랍의 비밀번호

---
+++ src/swea/Solution_2043.java
@@ -0,0 +1,16 @@
+package swea;
+
+import java.util.Scanner;
+
+public class Solution_2043 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int P, K;
+		P = sc.nextInt();
+		K = sc.nextInt();
+		
+		System.out.println(P-K+1);
+	}
+
+}
