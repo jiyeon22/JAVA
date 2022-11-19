506ed8a27f01a8b8843602bb5f73992d0298c8e1
jiyeon22 <kkll4106@naver.com>
2022-11-19 23:10:43 +0900

    그대로 출력하기

---
+++ src/baekjoon/B11718.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B11718 {
+	
+	
+	    public static void main(String[] args){
+	        Scanner sc = new Scanner(System.in);
+	        
+	        while(sc.hasNextLine()) {
+	        String s = sc.nextLine();
+	        System.out.println(s);
+	    }
+	}
+}
+
+
