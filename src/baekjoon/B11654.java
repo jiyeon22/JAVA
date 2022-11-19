c9190558b50fc136250bad1eec68e61923e74693
jiyeon22 <kkll4106@naver.com>
2022-11-19 19:44:52 +0900

    아스키 코드

---
+++ src/baekjoon/B11654.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		int num = str.charAt(0);
+		
+		System.out.println(num);
+	
+	}
+
+}
