206f26fff2c17b16c37487e272fe0b86d013e040
jiyeon22 <kkll4106@naver.com>
2022-11-19 21:58:17 +0900

    이상한 기호

---
+++ src/baekjoon/B15964.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B15964 {
+	
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long A = sc.nextInt();
+		long B = sc.nextInt();
+		
+		System.out.println((A+B) * (A-B));
+		
+		
+	}
+
+}
