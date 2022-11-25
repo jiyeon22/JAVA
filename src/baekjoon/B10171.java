c4e43ea6a02295c5f5e916b2fd9bee278d17a814
jiyeon22 <kkll4106@naver.com>
2022-11-25 13:31:31 +0900

    출력문제

---
+++ src/baekjoon/B10171.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+public class B10171 {
+	public static void main(String[] args) {
+		 System.out.println("\\    /\\");
+		 System.out.println(" )  ( ')");
+		 System.out.println("(  /  )");
+		 System.out.println(" \\(__)|");
+         
+
+
+
+
+
+	}
+
+}
