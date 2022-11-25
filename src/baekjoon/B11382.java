a5432b635d6a6360680d39e3879e8de0da662c31
jiyeon22 <kkll4106@naver.com>
2022-11-25 13:45:41 +0900

    구현 · 사칙연산 · 수학

---
+++ src/baekjoon/B11382.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class B11382 {
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		
+		long A= sc.nextLong();
+		long B = sc.nextLong();
+		long C = sc.nextLong();
+		
+		System.out.println(A+B+C);
+	}
+
+}
