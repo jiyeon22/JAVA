a9234b649c564884215dfbc7e5c03cd062cad010
jiyeon <kkll4106@naver.com>
2022-12-13 20:49:29 +0900

    배열_과제 안 내신분..?

---
+++ src/baekjoon/B5597.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B5597 {
+	public static void main(String[] args) {
+		int[] num = new int [31];
+		int[] num2 = new int [2];
+		int max = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 1; i <= 28; i++) {
+			int n = sc.nextInt();
+			num[n] = n;
+		}
+		
+		for(int i = 1; i <=30; i++) {
+			if(num[i] == 0) {
+				System.out.println(i);
+				}
+			
+			}
+			
+		}
+		
+		
+	}
+
+
